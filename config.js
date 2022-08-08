da1e0bd08899c8d989656ad2f206c302a2ef1de6
Hendraygy <106602437+Hendraygy@users.noreply.github.com>
2022-08-08 11:05:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -123,9 +123,9 @@ global.author = global.wm
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://telegra.ph/file/5444accedcd06f172899d.jpg' //Main Thumbnail
-global.thumb2 = 'https://telegra.ph/file/5444accedcd06f172899d.jpg'
-global.thumbbc = 'https://telegra.ph/file/d389f4acafac741f6592c.jpg' //For broadcast
+global.thumb = 'https://telegra.ph/file/18214c541337d8f5a475c.jpg' //Main Thumbnail
+global.thumb2 = 'https://telegra.ph/file/18214c541337d8f5a475c.jpg'
+global.thumbbc = 'https://telegra.ph/file/18214c541337d8f5a475c.jpg' //For broadcast
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
